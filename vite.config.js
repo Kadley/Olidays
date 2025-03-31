d27e9fbe6e8185b556f6002dbf1eada16414a3c4
Kadley <ton-email-associ--GitHub>
2025-03-31 18:27:53 +0200

    Ajout de pnpm-lock.yaml + corrections CI / vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
    // Permettra sur github pages, d'avoir le bon lien vers les fichiers css / js lorsque le github pages est public.
   // Quand un github pages est public, il est sur le lien https://<username>.github.io/<repo_name>
   // On va donc ajouter dans le base_url le nom du repo
-  base: process.env.BASE_URL,
+  base: process.env.BASE_URL || '/',
 })
