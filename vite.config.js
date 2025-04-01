5ab86591380a7824010831128f24cba7e3cf47c0
Kadley <ton-email-associ--GitHub>
2025-04-01 09:58:33 +0200

    correction

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   // Permettra sur github pages, d'avoir le bon lien vers les fichiers css / js lorsque le github pages est public.
+  // Permettra sur github pages, d'avoir le bon lien vers les fichiers css / js lorsque le github pages est public.
   // Quand un github pages est public, il est sur le lien https://<username>.github.io/<repo_name>
   // On va donc ajouter dans le base_url le nom du repo
   base: process.env.BASE_URL || '/',
-})
+});
